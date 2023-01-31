8e5893b0d53929834d22631accf3fe1c6beba049
Vasili Bulkin <bulkin@letterboxes.org>
2023-01-31 23:41:09 +0300

    eglot replacing lsp-mode, tramp a bit quicker.

--- load_packages.el
+++ load_packages.el
@@ -12,23 +12,27 @@
 (defvar used-packages
   '(ace-window
     autopair
+    counsel
     cuda-mode
     dash ;; for solarized-contrast
-    egg
+    eglot
     elpy
     go-mode
     fill-column-indicator
     haskell-mode
     jupyter
+    lua-mode
+    lsp-mode
     magit
+    meson-mode
     multiple-cursors
     paredit
     qml-mode
+    qt-pro-mode
     rustic
     slime
     smart-tabs-mode
-    sr-speedbar
-    lsp-mode))
+    sr-speedbar))
 
 (unless
     (cl-reduce (lambda (x y) (and x y))
@@ -41,9 +45,9 @@
     (unless (package-installed-p p)
       (package-install p))))
 
-(require 'egg)
+(add-to-list 'auto-mode-alist '("\\.pr[io]$" . qt-pro-mode))
 
-(elpy-enable)
+;;(elpy-enable)
 (setenv "JUPYTER_CONSOLE_TEST" "1")
 (setenv "IPY_TEST_SIMPLE_PROMPT" "1")
 
@@ -91,6 +95,7 @@
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 ;; paredit load hooks
+(require 'paredit)
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
@@ -98,6 +103,10 @@
 (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
 (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+;; fix paredit overriding RET in minibuffer
+(define-key paredit-mode-map (kbd "RET") nil)
+(define-key paredit-mode-map (kbd "C-j") 'paredit-newline)
+
 
 ;; Proper speedbar
 (require 'sr-speedbar)
@@ -127,29 +136,64 @@
   (fci-mode 1))
 
 (add-hook 'rustic-mode-hook 'my-rust-hook)
-
+(setq rustic-lsp-client 'eglot)
 
 ;; c++
 (add-to-list 'auto-mode-alist '("\\.cuh?\\'" . c++-mode))
-(require 'lsp-mode)
-(add-hook 'c-mode-hook 'lsp)
-(add-hook 'c++-mode-hook 'lsp)
-(lsp-register-client
- (make-lsp-client :new-connection
-                  (lsp-tramp-connection "clangd")
-                     :major-modes '(c++-mode c-mode)
-                     :remote? t
-                     :server-id 'clangd-remote))
+;; (require 'lsp-mode)
+;; (add-hook 'c-mode-hook 'lsp)
+;; (add-hook 'c++-mode-hook 'lsp)
+;; (lsp-register-client
+;;  (make-lsp-client :new-connection
+;;                   (lsp-tramp-connection "clangd")
+;;                      :major-modes '(c++-mode c-mode)
+;;                      :remote? t
+;;                      :server-id 'clangd-remote))
+(setq lsp-clients-clangd-args '("--fallback-style=WebKit"))
+
+;; use eglot instead of lsp for better tramp performance
+(add-hook 'c-mode-hook 'eglot-ensure)
+(add-hook 'c++-mode-hook 'eglot-ensure)
+(add-hook 'python-mode-hook 'eglot-ensure)
+(add-hook 'go-mode 'eglot-ensure )
+
+(defun clangd-switch-source-header ()
+  (interactive)
+  (let* ((res
+          (jsonrpc-request (eglot--current-server-or-lose)
+                           :textDocument/switchSourceHeader
+                           (eglot--TextDocumentIdentifier)))
+         (related-file (eglot--uri-to-path res)))
+    (message "Opening %s" res)
+    (if (string= "" related-file)
+        '()
+      (find-file related-file))))
+
 (defun my-c-hook ()
-  (local-set-key "\C-ch" 'lsp-clangd-find-other-file)
+  ;;(local-set-key "\C-ch" 'ff-find-other-file)
+  (local-set-key "\C-ch" 'clangd-switch-source-header)
   (local-set-key "\C-cC" 'compile)
   (local-set-key "\C-ca" 'align)
+  (local-set-key "\C-c\C-f" 'counsel-imenu)
+  ;;(add-to-list cc-search-directories (concat (project-root (project-current t)) "/include"))
+  ;;(local-set-key [tab] (lambda () (interactive) (eglot-format (point-at-bol) (point-at-eol))))
   (autopair-mode 1)
   (auto-fill-mode 0)
   (yas-minor-mode 1)
   (fci-mode 1))
 
-(add-hook 'c-mode-hook 'my-c-hook)
+(add-hook 'eglot-managed-mode-hook
+          (lambda ()
+            ;; Show flymake diagnostics first.
+            (setq eldoc-documentation-functions
+                  (cons #'flymake-eldoc-function
+                        (remove #'flymake-eldoc-function eldoc-documentation-functions)))
+            ;; Show all eldoc feedback.
+            (setq eldoc-documentation-strategy #'eldoc-documentation-compose)))
+
+(add-hook 'c-mode-hook (lambda ()
+                         (let ((lsp-enable-indentation nil))
+                           (my-c-hook))))
 (add-hook 'c++-mode-hook 'my-c-hook)
 
 (defun lsp-restart ()
